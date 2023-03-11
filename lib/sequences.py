719fa8c30e70a27135fa54d5f35505ab01e16776
kadimasum <kadimasam@gmail.com>
2023-03-11 06:28:48 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    n1, n2 = 0,1
+    count = 0
+    result = []
+
+    if length == 0: 
+        print(result)
+    if length == 1:
+        print(n1)
+    else:
+        while count < length:
+            result.append(n1)
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+            count += 1
+    print(result)
+    
